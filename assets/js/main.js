b969dbe94e15aaf87ef9ecf666db23d3fbf9a4d9
Beatitude <beatitude102@gmail.com>
2026-07-17 11:39:28 +0100

    last commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,7 @@
       if (target.length) {
         e.preventDefault();
 
+        
         if ($(this).parents('.nav-menu, .mobile-nav').length) {
           $('.nav-menu .active, .mobile-nav .active').removeClass('active');
           $(this).closest('li').addClass('active');
